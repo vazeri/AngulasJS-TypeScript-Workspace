a1d1e18e70191aa34f57e5dba7cbeeaa4b385961
Erick Vazquez <gzerox@gmail.com>
2018-01-16 06:22:31 -0600

    TemplateURL + body.component
    Separando el body de los componenters por medio de templateURL

--- AngularJS/02_Plantilla_Local/src/app/app.module.ts
+++ AngularJS/02_Plantilla_Local/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HeaderComponent }from "./components/header.component"
+import { BodyComponent } from './components/body.component';
 
 import { AppComponent } from './app.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
--- AngularJS/02_Plantilla_Local/src/app/components/header.component.ts
+++ AngularJS/02_Plantilla_Local/src/app/components/header.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `
-  <h1> Menu bootstrap </h1>
-  `
+  templateUrl: 'header.component.html'
+
 })
 export class HeaderComponent {
 }
