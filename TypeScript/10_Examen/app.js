cd9d55646f77e429d1969fddf5dd064e85cb760a
Erick Vazquez <gzerox@gmail.com>
2018-01-16 04:33:09 -0600

    Resumen
    resumen/ejercicio para simentar los temas repasados

---
+++ TypeScript/10_Examen/app.js
@@ -0,0 +1,8 @@
+//Nombre debe ser una variable tipo string (texto)
+function saludar(nombre) {
+    console.log("Hola" + nombre.toLowerCase());
+}
+//Objeto con propiedades de la variable
+var erick = {
+    nombre: "Goat"
+};
